1a3bbace1624b11244b6341fa8e92dd95f855fb9
Alexandra Severiukhina <aseveriukhina@griddynamics.com>
2019-01-28 17:06:51 +0400

    setup structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+
+import Navbar from './component/Navbar';
+import ProductList from './component/ProductList';
+import Cart from './component/Cart';
+import Default from './component/Default';
+import Details from "./component/Details";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <React.Fragment>
+            <Navbar/>
+            <ProductList/>
+            <Details/>
+            <Default/>
+        </React.Fragment>
     );
   }
 }
