3ebf93dc8ddbf6924378068d7129100af7cbb059
Alexandra Severiukhina <aseveriukhina@griddynamics.com>
2019-01-28 18:43:11 +0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom'
 import './App.css';
-import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar from './component/Navbar';
 import ProductList from './component/ProductList';
@@ -13,9 +14,12 @@ class App extends Component {
     return (
         <React.Fragment>
             <Navbar/>
-            <ProductList/>
-            <Details/>
-            <Default/>
+            <Switch>
+                <Route exact path='/' component={ProductList}/>
+                <Route path='/details' component={Details}/>
+                <Route path='/cart' component={Cart}/>
+                <Route component={Default}/>
+            </Switch>
         </React.Fragment>
     );
   }
