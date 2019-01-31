27dcdaedeff7cbf88ad6a4353840324bae567983
Alexandra Severiukhina <aseveriukhina@griddynamics.com>
2019-01-31 14:43:22 +0400

    details page markup

--- src/component/Details.jsx
+++ src/component/Details.jsx
@@ -1,13 +1,62 @@
 import React, {Component} from 'react';
+import {ProductConsumer} from "../context";
+import {Link} from "react-router-dom";
+import {ButtonContainer} from "./Button";
 
-class Details extends Component {
+export default class Details extends Component {
     render() {
         return (
-            <div>
-                Details
-            </div>
+           <ProductConsumer>
+               {(value)=>{
+                   const {id, title, img, company, inCart, price, info} = value.detailProduct;
+                   return (
+                       <div className='container py-5'>
+                        <div className="row">
+                            <div className="col-10 mx-auto text-blue text-center text-slanted my-5">
+                                <h1>{title}</h1>
+                            </div>
+                        </div>
+                           <div className="row">
+                               <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                                   <img src={img} className='img-fluid' alt='product'/>
+                               </div>
+                               <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                                 <h2>model: {title}</h2>
+                                   <h4 className='text-title text-muted text-uppercase mt-3 mb-2'>
+                                       made by: <span className='text-uppercase'>{company}</span>
+                                   </h4>
+                                   <h4 className='text-blue'>
+                                      <strong>
+                                          price: <span>$ {price}</span>
+                                      </strong>
+                                   </h4>
+                                   <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                                      some info about product
+                                   </p>
+                                   <p className='text-muted lead'>
+                                       {info}
+                                   </p>
+                                   {/*{buttons}*/}
+                                   <div>
+                                       <Link to='/'>
+                                           <ButtonContainer>
+                                               Back to Products
+                                           </ButtonContainer>
+                                       </Link>
+                                       <ButtonContainer
+                                           disabled={inCart} onClick={() => {value.addToCart(id)}}>
+                                           {inCart? 'InCart':'Add to cart'}
+                                       </ButtonContainer>
+                                   </div>
+
+                               </div>
+                           </div>
+                       </div>
+                   )
+
+               }}
+           </ProductConsumer>
         );
     }
 }
 
-export default Details;
--- src/context.js
+++ src/context.js
@@ -5,16 +5,33 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
     state ={
-        products:storeProducts,
+        products:[],
         detailProduct
+    }
+
+    componentDidMount() {
+        this.setProducts();
+    }
+
+    setProducts = () => {
+        let tempProducts = [];
+        storeProducts.forEach(item => {
+            const singleItem = {...item};
+            tempProducts = [...tempProducts, singleItem];
+        })
+
+        this.setState(() => {
+                return {products: tempProducts}
+            }
+        )
     }
 
     handleDetail = () =>{
         console.log('Detail');
     }
 
-    addToCart = () =>{
-        console.log('Add to cart');
+    addToCart = (id) =>{
+        console.log('Add to cart', id);
     }
     render() {
         return (
