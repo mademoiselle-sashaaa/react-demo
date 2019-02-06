af5440e3039f1d26003dee322f67816f85919626
Alexandra Severiukhina <aseveriukhina@griddynamics.com>
2019-02-06 17:38:02 +0400

    heroku trial

--- src/component/Default.jsx
+++ src/component/Default.jsx
@@ -9,7 +9,6 @@ export default class Default extends Component {
                        <h1 className='display-3'>
                            404
                        </h1>
-                       <h1>error</h1>
                        <h2>page not found</h2>
                        <h3>the requested url <span className='text-danger'>{this.props.location.pathname}</span> was not found</h3>
                    </div>
